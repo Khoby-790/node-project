7e7e75acc89f1fc00917457d75056e0e9ccb1566
Domey Ben <domeybenjamin1@gmail.com>
2019-09-19 02:46:34 +1200

    H

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,5 +1,3 @@
-import passport from 'passport';
-import mongoose from 'mongoose';
 import Utils from '../utils/index';
 import User from '../models/user.model';
 
@@ -7,27 +5,27 @@ import User from '../models/user.model';
 
 class AuthController {
 
-    static index(req, res, next){
+    static index(req, res, next) {
         res.render('login');
     }
 
 
-    static async register(req, res, next){
+    static async register(req, res, next) {
         const { email, password, password2, gender, name } = req.body;
 
         const user = await User.findOne({ email });
 
-        if(user){
-            req.flash('error_msg','email already exists');
+        if (user) {
+            req.flash('error_msg', 'email already exists');
             res.redirect('back');
         }
-        req.checkBody('email','provide a valid email address').notEmpty().isEmail();
-        req.checkBody('name','We could use a name you know?').notEmpty();
-        req.checkBody('password','Please provide a means of locking your account (Password)').notEmpty().length({min: 6, max: 12});
+        req.checkBody('email', 'provide a valid email address').notEmpty().isEmail();
+        req.checkBody('name', 'We could use a name you know?').notEmpty();
+        req.checkBody('password', 'Please provide a means of locking your account (Password)').notEmpty().length({ min: 6, max: 12 });
         req.checkBody('password2', 'Please provide a confirmation password').notEmpty().equals(password);
         const errors = req.validationErrors();
-        if(errors){
-            req.flash('errors',errors);
+        if (errors) {
+            req.flash('errors', errors);
             res.redirect('back');
         }
         const user = new User({
@@ -41,11 +39,9 @@ class AuthController {
 
         user.save();
 
-        res.flash('success_msg','Account created you can now login to your account');
+        res.flash('success_msg', 'Account created you can now login to your account');
         res.redirect('/login');
 
     }
 
 }
-
-
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,14 @@ const Route = Router();
 
 //controllers
 import HomeController from '../app/controllers/HomeController';
+import AuthController from '../app/controllers/AuthController';
 
 
 Route.route('/login')
     .get(HomeController.login);
 
 Route.route('/register')
-    .get(HomeController.register);
+    .get(HomeController.register)
+    .post(AuthController.register);
 
 module.exports = Route
